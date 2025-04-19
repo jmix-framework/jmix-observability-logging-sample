d596cc86dabfd24612a716c36a2e9a751fe8ebe5
Mario David <mario@die-davids.de>
2025-04-19 15:38:37 +0200

    loading fetchplan in list view while not in detail view

--- jmix-petclinic-portal/src/main/java/io/jmix/petclinic/portal/view/pet/PetListView.java
+++ jmix-petclinic-portal/src/main/java/io/jmix/petclinic/portal/view/pet/PetListView.java
@@ -2,18 +2,17 @@ package io.jmix.petclinic.portal.view.pet;
 
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.Route;
-import io.jmix.core.LoadContext;
+import io.jmix.core.security.CurrentAuthentication;
 import io.jmix.flowui.Fragments;
 import io.jmix.flowui.ViewNavigators;
+import io.jmix.flowui.model.CollectionContainer;
+import io.jmix.flowui.model.CollectionLoader;
 import io.jmix.flowui.view.*;
 import io.jmix.petclinic.portal.entity.Pet;
+import io.jmix.petclinic.portal.entity.User;
+import io.jmix.petclinic.portal.entity.Visit;
 import io.jmix.petclinic.portal.view.main.MainView;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.UUID;
 
 @Route(value = "pets", layout = MainView.class)
 @ViewController(id = "Pet.list")
@@ -27,6 +26,28 @@ public class PetListView extends StandardListView<Pet> {
     private Fragments fragments;
     @Autowired
     private ViewNavigators viewNavigators;
+    @ViewComponent
+    private CollectionLoader<Pet> petsDl;
+    @Autowired
+    private CurrentAuthentication currentAuthentication;
+    @ViewComponent
+    private CollectionContainer<Pet> petsDc;
+
+    @Subscribe
+    public void onInit(final InitEvent event) {
+        petsDl.setParameter("ownerId", currentUser().getOwnerId());
+    }
+
+    private User currentUser() {
+        return (User) currentAuthentication.getUser();
+    }
+
+    @Subscribe(id = "petsDc", target = Target.DATA_CONTAINER)
+    public void onPetsDcCollectionChange(final CollectionContainer.CollectionChangeEvent<Visit> event) {
+        cardWrapper.removeAll();
+        petsDc.getItems()
+                .forEach(this::addPetCard);
+    }
 
     private void addPetCard(Pet pet) {
         PetCard petCard = fragments.create(this, PetCard.class);
@@ -37,14 +58,6 @@ public class PetListView extends StandardListView<Pet> {
                 .navigate()
         );
         cardWrapper.add(petCard);
-    }
-
-    @Install(to = "petsDl", target = Target.DATA_LOADER)
-    protected List<Pet> petsDlLoadDelegate(LoadContext<Pet> loadContext) {
-        List<Pet> pets = new ArrayList<>();
-        cardWrapper.removeAll();
-        pets.forEach(this::addPetCard);
-        return pets;
     }
 
 }
