9bec3ad49935dbca2fbadf73601d5a7803a69bfd
Mario David <mario@die-davids.de>
2025-04-29 07:34:12 +0200

    smaller improvements

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-./gradlew -Pvaadin.productionMode=true --no-parallel --include-build jmix-petclinic-2 :jmix-petclinic-2:clean :jmix-petclinic-2:bootBuildImage -x test --no-build-cache
-./gradlew -Pvaadin.productionMode=true --no-parallel --include-build jmix-petclinic-portal :jmix-petclinic-portal:clean :jmix-petclinic-portal:bootBuildImage -x test --no-build-cache
+./gradlew -Pvaadin.productionMode=true --no-parallel --include-build jmix-petclinic-2 :jmix-petclinic-2:clean :jmix-petclinic-2:bootJar -x test --no-build-cache
+./gradlew -Pvaadin.productionMode=true --no-parallel --include-build jmix-petclinic-portal :jmix-petclinic-portal:clean :jmix-petclinic-portal:bootJar -x test --no-build-cache
